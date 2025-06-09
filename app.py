8b92da3a5d12225460165a86c3d52cd2077a7411
Mohammad Khusnul Khuluq <59632952+khulqu15@users.noreply.github.com>
2025-06-10 04:50:17 +0700

    add cors to app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import cv2
 from aiohttp import web
+from aiohttp.web_middlewares import middleware
 from aiortc import RTCPeerConnection, VideoStreamTrack, RTCSessionDescription
 from aiortc.contrib.media import MediaRelay
 from av import VideoFrame
@@ -7,6 +8,17 @@ import asyncio
 
 pcs = set()
 relay = MediaRelay()
+
+@middleware
+async def cors_middleware(request, handler):
+    response = await handler(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET,POST,OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    return response
+
+async def options_handler(request):
+    return web.Response(status=204)
 
 class CameraStream(VideoStreamTrack):
     def __init__(self):
@@ -43,6 +55,7 @@ async def offer(request):
         "type": pc.localDescription.type
     })
 
-app = web.Application()
+app = web.Application(middlewares=[cors_middleware])
 app.router.add_post("/offer", offer)
+app.router.add_options("/offer", options_handler)
 web.run_app(app, port=8080)
